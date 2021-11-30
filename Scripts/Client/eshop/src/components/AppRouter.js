c0e1e681b9bb742b219befe50ed25086f41b929d
Dexar <dima.mikhayevich@mail.ru>
2021-11-30 15:04:47 +0300

    1.23.36

--- Scripts/Client/eshop/src/components/AppRouter.js
+++ Scripts/Client/eshop/src/components/AppRouter.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, {createContext, useContext} from 'react';
 import {Switch, Route, Redirect} from "react-router-dom";
+import {authRoutes, publicRoutes} from "../routes";
+import {SHOP_ROUTE} from "../utils/consts";
+import {Context} from "../index";
 
 const AppRouter = () => {
+    const {user} = useContext(Context)
     return (
-        <div>
-            
-        </div>
+        <Switch>
+            {user.isAuth && authRoutes.map(({path,Component})=>
+                <Route key={path} path={path} component={Component} exact/>
+            )}
+            {publicRoutes.map(({path,Component})=>
+                <Route key={path} path={path} component={Component} exact/>
+            )}
+            <Redirect to={SHOP_ROUTE}/>
+        </Switch>
     );
 };
 
